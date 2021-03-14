a2faa351f1a45d77a6e2942ea027f910866b01db
jaxmerius <ethan@bfitec.com>
2021-03-13 18:59:56 -0700

    Mobile Contact Page

--- pages/contact.js
+++ pages/contact.js
@@ -8,7 +8,7 @@ export default function Contact() {
       <Nav currentPage="Contact" />
       <div className="bg-green-200 h-full">
         <div className="flex justify-center">
-          <div className="grid grid-cols-3 gap-8 pt-36 w-2/3 font-bold text-lg">
+          <div className="grid grid-cols-3 gap-2 sm:gap-8 pt-36 w-3/4 sm:w-2/3 font-bold text-lg">
             <div className="bg-white rounded-lg shadow-lg text-center my-auto">
               <a href="mailto:mathes974@gmail.com">
                 <img className="w-1/2 mx-auto" src="/mail.png" alt="mail" />
