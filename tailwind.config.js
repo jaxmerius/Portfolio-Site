78cadd9a01ca3ea1309284acbe8a96877c8d2b1a
jaxmerius <ethan@bfitec.com>
2021-06-03 17:43:35 -0600

    removed tailwind purging due to bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./styles/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: [],
   darkMode: false,
   theme: {
     extend: {
